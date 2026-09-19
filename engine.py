de32bd59fc14ccfdf3deac077a1ac0683bf3719f
Vimal3663 <vimalraj6114@gmail.com>
2026-09-19 18:59:05 +0530

    Delete engine.py

--- engine.py
+++
@@ -1,7 +0,0 @@
-from app.exceptions import PolicyError
-
-class PolicyEngine:
-    def check(self, tool, confirmed=False):
-        if tool.get('requires_confirmation') and not confirmed:
-            return {'allowed':False,'requires_confirmation':True,'reason':f"'{tool['name']}' is classified as {tool.get('risk')} and requires user confirmation."}
-        return {'allowed':True,'requires_confirmation':False}
